3f552b3b785fc988879fe1c80d1a464e033a1e68
bigrocs <bigrocs@qq.com>
2020-04-19 17:38:44 +0800

    增加支付服务器连接

--- src/renderer/api/pay.js
+++ src/renderer/api/pay.js
@@ -1,10 +1,21 @@
 import request from '@/utils/request'
+import store from '@/store'
 export function AopF2F(data) {
-  return request({
-    url: '/pay-api/pays/aopF2F',
-    method: 'post',
-    data: {
-      order: data
+  return new Promise((resolve, reject) => {
+    if (store.state.healthy.isServer) {
+      request({
+        url: '/pay-api/pays/aopF2F',
+        method: 'post',
+        data: {
+          order: data
+        }
+      }).then(response => {
+        resolve(response)
+      }).catch(error => {
+        reject(error)
+      })
+    } else {
+      reject(new Error('支付服务器无法链接'))
     }
   })
 }
