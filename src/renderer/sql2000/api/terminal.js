b1391cc5133adf5912261d1a431d629b15f733a4
bigrocs <bigrocs@qq.com>
2020-05-16 16:20:02 +0800

    修复不自动更新问题

--- src/renderer/sql2000/api/terminal.js
+++ src/renderer/sql2000/api/terminal.js
@@ -11,78 +11,82 @@ import { Loading } from 'element-ui'
  * @ 默认 30 秒 更新一次 path src/renderer/App.vue
  */
 export async function SyncTerminal() {
-  Terminal.PosCode = store.state.settings.terminal
-  if (Terminal.PosCode) {
-    store.dispatch('terminal/handerSyncTerminal', false) // 关闭自动同步
-    await Terminal.Get()
-    let status = 0 // 等待窗口锁
-    // 更新用户信息
-    if (Terminal.IsChgUser === '1') {
-      status++
-      const loadingInstance = Loading.service({
-        text: '更新用户信息中...',
-        background: 'rgba(0, 0, 0, 0.7)'
-      })
-      await SyncUser().then(() => {
-        Terminal.IsChgUser = '0'
-        Terminal.Save()
-        loadingInstance.text = '用户信息更新完成'
-        setTimeout(() => {
-          status--
-          if (status === 0) {
-            loadingInstance.close()
-          }
-        }, 1000)
-      }).catch(error => {
-        console.log(error)
-      })
+  try {
+    Terminal.PosCode = store.state.settings.terminal
+    if (Terminal.PosCode) {
+      store.dispatch('terminal/handerSyncTerminal', false) // 关闭自动同步
+      await Terminal.Get()
+      let status = 0 // 等待窗口锁
+      // 更新用户信息
+      if (Terminal.IsChgUser === '1') {
+        status++
+        const loadingInstance = Loading.service({
+          text: '更新用户信息中...',
+          background: 'rgba(0, 0, 0, 0.7)'
+        })
+        await SyncUser().then(() => {
+          Terminal.IsChgUser = '0'
+          Terminal.Save()
+          loadingInstance.text = '用户信息更新完成'
+          setTimeout(() => {
+            status--
+            if (status === 0) {
+              loadingInstance.close()
+            }
+          }, 1000)
+        }).catch(error => {
+          console.log(error)
+        })
+      }
+      // 更新商品信息
+      if (Terminal.IsChgPlu === '1') {
+        status++
+        const loadingInstance = Loading.service({
+          text: '更新商品信息中...',
+          background: 'rgba(0, 0, 0, 0.7)'
+        })
+        await SyncPlu().then(() => {
+          Terminal.IsChgPlu = '0'
+          Terminal.Save()
+          loadingInstance.text = '商品信息更新完成'
+          setTimeout(() => {
+            status--
+            if (status === 0) {
+              loadingInstance.close()
+            }
+          }, 1000)
+        }).catch(error => {
+          console.log(error)
+        })
+      }
+      // 更新支付信息
+      if (Terminal.IsChgZfKind === '1') {
+        status++
+        const loadingInstance = Loading.service({
+          text: '更新支付信息中...',
+          background: 'rgba(0, 0, 0, 0.7)'
+        })
+        await SyncPay().then(() => {
+          Terminal.IsChgZfKind = '0'
+          Terminal.Save()
+          loadingInstance.text = '支付信息更新完成'
+          setTimeout(() => {
+            status--
+            if (status === 0) {
+              loadingInstance.close()
+            }
+          }, 1000)
+        }).catch(error => {
+          console.log(error)
+        })
+      }
+      store.dispatch('terminal/handerSyncTerminal', true) // 开启自动同步
+      Terminal.UserName = store.getters.name
+      Terminal.UserCode = store.getters.username
+      Terminal.PosState = '10'
+      Terminal.Save()
     }
-    // 更新商品信息
-    if (Terminal.IsChgPlu === '1') {
-      status++
-      const loadingInstance = Loading.service({
-        text: '更新商品信息中...',
-        background: 'rgba(0, 0, 0, 0.7)'
-      })
-      await SyncPlu().then(() => {
-        Terminal.IsChgPlu = '0'
-        Terminal.Save()
-        loadingInstance.text = '商品信息更新完成'
-        setTimeout(() => {
-          status--
-          if (status === 0) {
-            loadingInstance.close()
-          }
-        }, 1000)
-      }).catch(error => {
-        console.log(error)
-      })
-    }
-    // 更新支付信息
-    if (Terminal.IsChgZfKind === '1') {
-      status++
-      const loadingInstance = Loading.service({
-        text: '更新支付信息中...',
-        background: 'rgba(0, 0, 0, 0.7)'
-      })
-      await SyncPay().then(() => {
-        Terminal.IsChgZfKind = '0'
-        Terminal.Save()
-        loadingInstance.text = '支付信息更新完成'
-        setTimeout(() => {
-          status--
-          if (status === 0) {
-            loadingInstance.close()
-          }
-        }, 1000)
-      }).catch(error => {
-        console.log(error)
-      })
-    }
+  } catch (err) {
     store.dispatch('terminal/handerSyncTerminal', true) // 开启自动同步
-    Terminal.UserName = store.getters.name
-    Terminal.UserCode = store.getters.username
-    Terminal.PosState = '10'
-    Terminal.Save()
   }
 }
